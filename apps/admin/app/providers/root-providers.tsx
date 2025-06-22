ba185842049acf436d5c25335a17caa0bd7ec7db
TinsFox <fox@tinsfox.com>
2025-06-23 00:00:20 +0800

    feat: integrate PostHog for analytics tracking

--- apps/admin/app/providers/root-providers.tsx
+++ apps/admin/app/providers/root-providers.tsx
@@ -3,6 +3,7 @@ import { ToasterPrimitive } from "@poketto/ui/toaster";
 import { TooltipProvider } from "@poketto/ui/tooltip";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { LazyMotion, MotionConfig } from "framer-motion";
+import posthog from "posthog-js";
 import { PostHogProvider } from "posthog-js/react";
 import type { FC, PropsWithChildren } from "react";
 import { HotkeysProvider } from "react-hotkeys-hook";
@@ -19,16 +20,15 @@ import { TailwindIndicator } from "~/components/tailwind-indicator";
 const loadFeatures = () =>
 	import("../framer-lazy-feature").then((res) => res.default);
 
+if (process.env.NODE_ENV !== "development") {
+	posthog.init(env.VITE_PUBLIC_POSTHOG_KEY, {
+		api_host: env.VITE_PUBLIC_POSTHOG_HOST,
+	});
+}
+
 export const RootProviders: FC<PropsWithChildren> = ({ children }) => (
 	<I18nextProvider i18n={i18n}>
-		<PostHogProvider
-			apiKey={env.VITE_PUBLIC_POSTHOG_KEY}
-			options={{
-				api_host: env.VITE_PUBLIC_POSTHOG_HOST,
-				debug: import.meta.env.DEV,
-				autocapture: import.meta.env.PROD,
-			}}
-		>
+		<PostHogProvider client={posthog}>
 			<LazyMotion features={loadFeatures} strict key="framer">
 				<MotionConfig
 					transition={{
